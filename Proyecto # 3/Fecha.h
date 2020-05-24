814dd2266d3a40efef1b8821cb684e7fb7b036f5
Juan <55890486+SinCuello@users.noreply.github.com>
2020-05-24 15:23:59 -0500

    Segundo commit
    Hice la sobrecarga de objetos me falto los de >> y <<

--- Proyecto # 3/Fecha.h
+++ Proyecto # 3/Fecha.h
@@ -1,4 +1,7 @@
 
+
+#include <iostream>
+using namespace std;
 class Fecha
 {
 private:
@@ -16,16 +19,119 @@ public:
     void setAa(int _aa){aa=_aa;};
     int getAa(){return aa;};
 
-    Fecha suma(Fecha c2);
+    void mostrarFecha();
+
+    Fecha operator+(Fecha c2);
+    int operator>=(Fecha c2);
+    int operator<=(Fecha c2);
+    int operator>(Fecha c2);
+    int operator<(Fecha c2);
+    int operator==(Fecha c2);
+
 };
 
-Fecha Fecha::suma(Fecha c2)
+Fecha::Fecha()
 {
-    int m2[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-    int i,k=0,p,a;
-    for(i=0; i<m i++)
+    dd=0;
+    mm=0;
+    aa=0;
+}
+Fecha Fecha::operator+(Fecha c2)
+{
+    //Number of  days I want add is c2 "ne"
+    int m2[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+    int k=0;// donde se storea la nueva fecha
+    int p,a;
+    for(int i=0; i<mm; i++)
     {
-        k=k+m2[i];//AQUI SUMA EL SUBINDICE HASTA LLEGAR AL DIA
-        k=k+dd;Aqui suma el dia
+        k=k+m2[i];
+        k=k+dd+c2.dd;
     }
+    cout<<"Nueva fecha"<<endl;
+    if(k<=365)
+    {
+        for(int i=0; i<13; i++)
+        {
+            p=k-m2[i];
+            if(p<=m2[i+1])
+            {
+                a=i+1;break;
+            }
+            else
+            {
+              k=p;
+            }
+        }
+        Fecha nueva(p,a,aa);
+        return nueva;
+    }
+    else
+    {
+        k=k-365;
+        for(int i=0; i<13;i++)
+        {
+            p=k-m2[i];
+            if(p<=m2[i+1])
+            {
+                a=i+1;break;
+            }
+            else
+            {
+                k=p;
+            }
+            Fecha nueva(p,a,aa+1);
+        }
+    }
+}
+
+int Fecha::operator>=(Fecha c2)
+{
+    if(dd>=c2.dd && mm>=c2.mm && aa>=c2.aa)
+        return 1;
+    else
+        return 1;
+}
+
+int Fecha::operator<=(Fecha c2)
+{
+    if(dd<=c2.dd && mm<=c2.mm && aa<=c2.aa)
+        return 1;
+    else
+        return 0;
+}
+
+int Fecha::operator>(Fecha c2)
+{
+    if(dd>c2.dd && mm>c2.mm && aa>c2.aa)
+        return 1;
+    else
+        return 0;
+}
+
+int Fecha::operator<(Fecha c2)
+{
+    if(dd<c2.dd && mm<c2.mm && aa<c2.aa)
+        return 1;
+    else
+        return 0;
+}
+
+int Fecha::operator==(Fecha c2)
+{
+    if(dd==c2.dd && mm==c2.mm && aa==c2.aa)
+        return 1;
+    else
+        return 0;
+}
+
+Fecha::Fecha(int _dd, int _mm, int _aa)
+{
+    dd=_dd;
+    mm=_mm;
+    aa=_aa;
+}
+
+void Fecha::mostrarFecha()
+{
+    cout<<"La fecha es "<<dd<<"/"<<mm<<"/"<<aa<<endl;
 }
--- Proyecto # 3/main.cpp
+++ Proyecto # 3/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include "Fecha.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Fecha hoy(9,1,2025);
+    Fecha hoy2(25,1,2030);
+    hoy.mostrarFecha();
+    hoy2.mostrarFecha();
+    if(hoy>=hoy2)
+    {
+        cout<<"hoy es mayor"<<endl;
+    }
+    else
+    {
+        cout<<"hoy es menor"<<endl;
+    }
     return 0;
 }
